1bf80a7131aaa9407eb267982016ce610375bbae
JulioDev1 <juliocesargualberto617@gmail.com>
2025-03-09 12:47:16 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BudgetPageComponent } from './page/budget-page/budget-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'budget-page',
+    component: BudgetPageComponent,
+}];
---
+++ src/app/page/budget-page/budget-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+
+@Component({
+  selector: 'app-budget-page',
+  imports: [HeaderComponent],
+  templateUrl: './budget-page.component.html',
+  styleUrl: './budget-page.component.css'
+})
+export class BudgetPageComponent {
+
+}
